0806bec1be5dcd8551d8bfd3b0868326bb115ec9
Brinza Gabriel <gabrielbrinza83@gmail.com>
2025-10-17 11:37:41 +0300

    Lab3

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import random
-lam_posibil = [1, 2, 5, 10]
-
-poisson_random_1 = np.random.poisson(lam=1, size=1000)
-print(poisson_random_1 + [1, 2])
